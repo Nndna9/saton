cae120a7d0c7783e6c37fce06389fbd78f2c8e19
Nandana Santhosh <119744179+Nndna9@users.noreply.github.com>
2023-06-07 15:26:20 +0000

    set homepage for github pages deploy

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an Aspiring AI engineer studying at Nitte Institute of Professional Education. I love meeting new people and seeking opportunities to learn new things!";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C,C++,Java,Javascript,Python",
+  "AI",
+  "Event management",
+  "Communication and presentation skills",
+  "Leadership",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.";
 
 const About = () => {
   return (
